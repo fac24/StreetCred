b2e9f1de5de41d7d62085df41bc25a8f66774df0
alternadiva <85565743+alternadiva@users.noreply.github.com>
2022-07-18 20:58:29 +0100

    display user profile

--- pages/profiles/[id].js
+++ pages/profiles/[id].js
@@ -1,12 +1,44 @@
 import supabase from "../../utils/supabaseClient";
 
 function Profile(props) {
-  return <div>Profile</div>;
+  return (
+    <section>
+      <h1>Profile</h1>
+      <img src={props.profile[0].avatar_url} />
+      <h2>{props.profile[0].name}</h2>
+      <p>{props.profile[0].points} points</p>
+      <p>Location: {props.profile[0].location}</p>
+
+      <div>
+        <h3>Groups</h3>
+        <ul>
+          <li>Group 1</li>
+          <li>Group 2</li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>Borrowed Products</h3>
+        <ul>
+          <li>Product 1</li>
+          <li>Product 2</li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>Posted Products</h3>
+        <ul>
+          <li>Product 1</li>
+          <li>Product 2</li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export async function getStaticPaths() {
   const { data, error } = await supabase.from("profiles").select();
-  const profiles = data ? data : [notfound];
+  const profiles = data ? data : [0];
 
   const paths = profiles.map((profile) => ({
     params: { id: `${profile.id}` },
--- pages/profiles/notfound.js
+++
@@ -1,5 +0,0 @@
-function NotFound() {
-  return <div>No profile found</div>;
-}
-
-export default NotFound;
