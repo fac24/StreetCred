5181a2739162618782d6cbfd92da29250cacf995
alternadiva <85565743+alternadiva@users.noreply.github.com>
2022-07-18 20:26:13 +0100

    set static path for dynamic routes
    Co-authored-by: Petra <alternadiva@users.noreply.github.com>
    Co-authored-by: Sonia <sonianb@users.noreply.github.com>

---
+++ pages/profiles/[id].js
@@ -0,0 +1,27 @@
+import supabase from "../../utils/supabaseClient";
+
+function Profile(props) {
+  return <div>Profile</div>;
+}
+
+export async function getStaticPaths() {
+  const { data, error } = await supabase.from("profiles").select();
+  const profiles = data ? data : [notfound];
+
+  const paths = profiles.map((profile) => ({
+    params: { id: `${profile.id}` },
+  }));
+
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps(context) {
+  const { data, error } = await supabase
+    .from("profiles")
+    .select()
+    .eq("id", context.params.id);
+
+  return { props: { profile: data } };
+}
+
+export default Profile;
---
+++ pages/profiles/notfound.js
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <div>No profile found</div>;
+}
+
+export default NotFound;
