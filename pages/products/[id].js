aad691c9bcdcd6901564dbaa678128f1cdf27cf9
Minju25Kim <48757517+minju25kim@users.noreply.github.com>
2022-07-19 13:05:29 +0100

    Co-authored-by: Asmahan <AsmahanM@users.noreply.github.com>

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -9,4 +9,24 @@ function Product(props) {
   );
 }
 
+export async function getStaticPaths() {
+  const { data, error } = await supabase.from("profiles").select();
+  const profiles = data ? data : [0];
+
+  const paths = profiles.map((profile) => ({
+    params: { id: `${profile.id}` },
+  }));
+
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps(context) {
+  const { data, error } = await supabase
+    .from("products")
+    .select()
+    .eq("id", context.params.id);
+
+  return { props: { profile: data } };
+}
+
 export default Product;
