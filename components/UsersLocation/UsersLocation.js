aa31bc6bea49e888ed47e2bfe04929e7a81975a4
sonianb <sonia.botsarova@gmail.com>
2022-07-19 16:53:40 +0100

    Merge branch
    Co-authored-by: Minju Kim <minju25kim@users.noreply.github.com>
    Co-authored-by: Petra <alternadiva@users.noreply.github.com>

--- components/UsersLocation/UsersLocation.js
+++ components/UsersLocation/UsersLocation.js
@@ -1,25 +1,52 @@
-// import React from "react";
+import { useState } from "react";
 
 function UsersLocation() {
-  //   const [locError, setLocError] = useState(false);
+  const [locError, setLocError] = useState(false);
+  const [postcode, setPostcode] = useState("");
+  async function handleSearchFromLocation(pos) {
+    //extracts longitude and latitude from geolocation info from locator function
+    const crd = pos.coords;
+    let lat = crd.latitude;
+    let lon = crd.longitude;
 
-  //   function geoError(err) {
-  //     setLocError(true);
-  //     console.warn(`ERROR(${err.code}): ${err.message}`);
-  //     return;
-  //   }
+    //uses postcodes API to convert user's coordinates to a postcode
+    const postcode = await fetch(
+      `https://api.postcodes.io/postcodes?lon=${lon}&lat=${lat}`
+    );
 
-  //   //get's location of user's device and takes success and error callback functions as parameters
-  //   function locator() {
-  //     setLocError(false);
-  //     navigator.geolocation.getCurrentPosition(
-  //       handleSearchFromLocation,
-  //       geoError
-  //     );
-  // onClick={locator}
+    const postJson = await postcode.json();
+    let userPostcode = postJson.result[0].postcode;
+
+    setPostcode(userPostcode);
+  }
+
+  function geoError(err) {
+    setLocError(true);
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+    return;
+  }
+
+  //get's location of user's device and takes success and error callback functions as parameters
+
+  function locator() {
+    setLocError(false);
+    navigator.geolocation.getCurrentPosition(
+      handleSearchFromLocation,
+      geoError
+    );
+  }
   return (
     <>
-      <button type="button">use current location</button>
+      <label htmlFor="search-location">Where do you live?</label>
+      <input
+        type="search"
+        placeholder="Enter your postcode or area"
+        value={postcode}
+        onChange={(event) => setPostcode(event.target.value)}
+      ></input>
+      <button type="button" onClick={locator}>
+        Use my current location
+      </button>
     </>
   );
 }
