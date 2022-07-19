75844f01c8bd239fa0febd635f8d337e19890f15
alternadiva <85565743+alternadiva@users.noreply.github.com>
2022-07-19 22:31:06 +0100

    redirect to group page on mouse down

--- components/Groups/ListGroups.js
+++ components/Groups/ListGroups.js
@@ -1,13 +1,28 @@
+import { useRouter } from "next/router";
 import supabase from "../../utils/supabaseClient";
 supabase;
 
 function ListGroups(props) {
+  const router = useRouter();
+
+  /*   function redirectToPage(event) {
+    event.preventDefault();
+    router.push(`/groups/${id}`);
+    console.log(event);
+  } */
+
   return (
     <div>
       <ul>
         {props.groups.map((group) => {
           return (
-            <li key={group.id}>
+            <li
+              key={group.id}
+              onMouseDown={(event) => {
+                event.preventDefault();
+                router.push(`/groups/${group.id}`);
+              }}
+            >
               <p>{group.name}</p>
               <img src={group.avatar} />
             </li>
