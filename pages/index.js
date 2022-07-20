f52151a038918964dbad842091800395a6c0de65
sonianb <sonia.botsarova@gmail.com>
2022-07-20 08:24:41 +0100

    add current location to landing page
    Co-authored-by: Minju Kim <minju25kim@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-import UsersLocation from "../components/UsersLocation/UsersLocation";
+import CurrentLocation from "../components/CurrentLocation/CurrentLocation";
 
 function Home() {
-  return <UsersLocation />;
+  return (
+    <>
+      <label htmlFor="search-location">Where do you live?</label>
+      <CurrentLocation />
+    </>
+  );
 }
 
 export default Home;
