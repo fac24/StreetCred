9d9ef5ab9b8568e76b99c71f457cd3cebd53f3a3
Minju25Kim <48757517+minju25kim@users.noreply.github.com>
2022-07-21 16:50:43 +0100

    update user's location by setting user data
    Co-authored-by: Sonia <sonianb@users.noreply.github.com>
    Co-authored-by: Asmahan <AsmahanM@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CurrentLocation from "../components/CurrentLocation/CurrentLocation";
-import supabase from "../utils/supabaseClient.js";
 import { useRouter } from "next/router";
 import { Link } from "next/link";
+import supabase from "../utils/supabaseClient";
 
-function Home() {
+function Home({ props }) {
+  const [userId, setUserId] = useState();
+  const [userData, setUserData] = useState();
+  const user = supabase.auth.user();
+
+  useEffect(() => {
+    for (const key in user) {
+      if (key == "id") {
+        setUserId(user[key]);
+      }
+      // console.log(`${key}: ${user[key]}`);
+    }
+
+    async function getLoc() {
+      const { data, error } = await supabase
+        .from("profiles")
+        .select()
+        .eq("id", userId);
+
+      setUserData(data[0].location);
+    }
+
+    getLoc();
+  }, []);
+
+  // check supabase currentuser location
+  //1 if they have location
+  // render the group they have joined
+
+  //2 if they dont' have location
+  // redirect them to profile setting page
+
   return (
     <>
       <label htmlFor="search-location">Where do you live?</label>
+      <p>{userId}</p>
       <CurrentLocation />
     </>
   );
 }
+
+/* export async function getStaticProps() {
+  const user = await supabase.auth.user();
+  const { data, error } = await supabase
+    .from("profiles")
+    .select()
+    .eq("id", user.id);
+
+  return { props: { profile: data } };
+} */
 
 export default Home;
