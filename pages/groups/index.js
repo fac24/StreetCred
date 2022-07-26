7aa1bbec456ebb1498209bcce42cfdf58a2fcaff
sonianb <sonia.botsarova@gmail.com>
2022-07-26 10:41:45 +0100

    show only groups that users are members of

--- pages/groups/index.js
+++ pages/groups/index.js
@@ -3,12 +3,27 @@ import AddNewGroupButton from "../../components/Groups/AddNewGroupButton";
 import FilterGroups from "../../components/Groups/FilterGroups";
 
 import supabase from "../../utils/supabaseClient";
+import { useState, useEffect } from "react";
+import { useAuthContext } from "../../context/auth";
 
 function Groups(props) {
+  const { user } = useAuthContext();
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      setGroups(
+        props.groups.filter((group) => group.members.includes(user.id))
+      );
+    }
+  }, [user]);
+
+  console.log(props.groups, groups, user?.id);
+
   return (
     <div>
       <FilterGroups />
-      <ListGroups groups={props.groups} />
+      <ListGroups groups={groups} />
       <AddNewGroupButton />
     </div>
   );
