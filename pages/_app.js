7b8157f35080b27b53867c603a95b2ee73f1c6a8
sonianb <sonia.botsarova@gmail.com>
2022-07-26 16:19:11 +0100

    put back removed authenticatdState

--- pages/_app.js
+++ pages/_app.js
@@ -11,10 +11,39 @@ import supabase from "../utils/supabaseClient";
 import { AuthWrapper, useAuthContext } from "../context/auth";
 
 function MyApp({ Component, pageProps }) {
-  // const [authenticatedState, setAuthenticatedState] =
-  //   useState("not-authenticated");
+  const [authenticatedState, setAuthenticatedState] =
+    useState("not-authenticated");
 
-  // const router = useRouter();
+  const router = useRouter();
+
+  useEffect(() => {
+    /* fires when a user signs in or out */
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        handleAuthChange(event, session);
+        if (event === "SIGNED_IN") {
+          setAuthenticatedState("authenticated");
+        }
+        if (event === "SIGNED_OUT") {
+          setAuthenticatedState("not-authenticated");
+          router.push("/login");
+        }
+      }
+    );
+
+    checkUser();
+    return () => {
+      authListener.unsubscribe();
+    };
+  }, []);
+
+  async function checkUser() {
+    /* when the component loads, checks user to show or hide Sign In link */
+    const user = await supabase.auth.user();
+    if (user) {
+      setAuthenticatedState("authenticated");
+    }
+  }
 
   async function handleAuthChange(event, session) {
     /* sets and removes the Supabase cookie */
