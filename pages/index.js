42533e1b16e79106f8357f37d8a8ff5d5d447216
alternadiva <85565743+alternadiva@users.noreply.github.com>
2022-07-27 16:31:28 +0100

    navbar web and mobile

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,9 @@ import "../styles/Home.module.css";
 import LandingWeb from "../components/About/LandingWeb";
 import LandingMobile from "../components/About/LandingMobile";
 import useViewport from "../components/Hooks/useViewport";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import supabase from "../utils/supabaseClient";
-import { useAuthContext } from "../context/auth";
 
 function Home() {
   const { width } = useViewport();
-  const auth = useAuthContext();
   const breakpoint = 620;
 
   return width < breakpoint ? <LandingMobile /> : <LandingWeb />;
