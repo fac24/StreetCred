a0679af3d61190ff7787b0a8e05463c9e13f708a
sonianb <sonia.botsarova@gmail.com>
2022-07-27 21:52:04 +0100

    fix build and deployment

--- pages/groups/add-group.js
+++ pages/groups/add-group.js
@@ -2,13 +2,12 @@ import React from "react";
 import GroupsForm from "../../components/Groups/GroupsForm";
 
 function AddGroup() {
-
   return (
     <section className="add-new-group-container">
       <h2>Create your group</h2>
       <GroupsForm />
     </section>
   );
-
+}
 
 export default AddGroup;
