7656250a7d88b27a108cea5600c3a6085a87f0de
alternadiva <85565743+alternadiva@users.noreply.github.com>
2022-07-27 17:17:33 +0100

    start on group page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import "../styles/Home.module.css";
 import LandingWeb from "../components/About/LandingWeb";
 import LandingMobile from "../components/About/LandingMobile";
 import useViewport from "../components/Hooks/useViewport";
+import { useEffect } from "react";
 
 function Home() {
   const { width } = useViewport();
