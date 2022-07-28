0e5a0012d78140bf81423116defac2afa778bb84
sonianb <sonia.botsarova@gmail.com>
2022-07-28 12:56:09 +0100

    add product distance feature

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -21,12 +21,11 @@ function Product(props) {
       const { data: owner, error } = await supabase
         .from("profiles")
         .select()
-        .eq("id", props.product[0].owner);
+        .eq("id", props.product.owner);
 
       setProductOwner(owner[0].name);
-      setProductOwnerId(props.product[0].owner);
+      setProductOwnerId(props.product.owner);
     }
-    //console.log(window.location.href);
 
     owner();
   }, [props.product]);
@@ -34,7 +33,7 @@ function Product(props) {
   async function createConversation() {
     const { data, error } = await supabase.from("conversations").insert([
       {
-        product_id: props.product[0].id,
+        product_id: props.product.id,
         owner_id: productOwnerId,
         requester_id: user.id,
       },
@@ -56,44 +55,83 @@ function Product(props) {
 
   return (
     <>
-      <h1>Products Page</h1>
-      <div>
-        <h3>{props.product[0].name}</h3>
-        <img src={props.product[0].image} />
-        <p>{props.product[0].description}</p>
-        <p>{props.product[0].location}</p>
-        <p>{props.product[0].created_at}</p>
+      <h2>Products Page</h2>
+      <div className="group-id-section">
+        <h3>{props.product.name}</h3>
+        <img src={props.product.image} className="group-page-avatar" />
+        <p>{props.product.description}</p>
+        {/* <p>{props.product.location}</p> */}
+        <p>Distance: {props.distance} km</p>
+        <p>Posted at: {props.product.created_at}</p>
         <ProductAvailability
-          availability={props.product[0].availability}
-          producId={props.product[0].id}
-          productOwner={props.product[0].owner}
+          availability={props.product.availability}
+          producId={props.product.id}
+          productOwner={props.product.owner}
         />
-        <button onClick={createConversation}>Contact {productOwner}!</button>
+        <button
+          onClick={createConversation}
+          className="open-group-button margin-bottom-sm"
+        >
+          Contact {productOwner}
+        </button>
+        <ShareLink />
+        <SocialShare />
       </div>
-      <ShareLink />
-      <SocialShare />
     </>
   );
 }
 
-export async function getStaticPaths() {
-  const { data, error } = await supabase.from("products").select();
-  const products = data ? data : ["022a7185-2aea-4d50-83e2-64229cd0f367"];
+export async function getServerSideProps({ req, params }) {
+  const { user } = await supabase.auth.api.getUserByCookie(req);
 
-  const paths = products?.map((product) => ({
-    params: { id: `${product.id}` },
-  }));
-
-  return { paths, fallback: true };
-}
-
-export async function getStaticProps(context) {
   const { data, error } = await supabase
     .from("products")
     .select()
-    .eq("id", context.params.id);
+    .eq("id", params.id);
 
-  return { props: { product: data } };
+  // return { props: { product: data } };
+
+  const userRequest = await supabase
+    .from("profiles")
+    .select()
+    .eq("id", user.id);
+
+  const distance = calcDistance(
+    userRequest.data[0].latitude,
+    userRequest.data[0].longitude,
+    data[0].latitude,
+    data[0].longitude,
+    "K"
+  );
+
+  return { props: { product: data[0], distance: Math.round(distance) } };
 }
 
 export default Product;
+
+function calcDistance(lat1, lon1, lat2, lon2, unit) {
+  if (lat1 == lat2 && lon1 == lon2) {
+    return 0;
+  } else {
+    var radlat1 = (Math.PI * lat1) / 180;
+    var radlat2 = (Math.PI * lat2) / 180;
+    var theta = lon1 - lon2;
+    var radtheta = (Math.PI * theta) / 180;
+    var dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit == "K") {
+      dist = dist * 1.609344;
+    }
+    if (unit == "N") {
+      dist = dist * 0.8684;
+    }
+    return dist;
+  }
+}
